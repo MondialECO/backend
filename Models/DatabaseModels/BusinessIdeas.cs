802938b06dec6c9c2b5b0f00633543299572a876
unknown <hrhafij8@gmail.com>
2026-01-10 23:42:09 -0800

    update idea collection

--- Models/DatabaseModels/BusinessIdeas.cs
+++ Models/DatabaseModels/BusinessIdeas.cs
@@ -6,18 +6,28 @@ namespace WebApp.Models.DatabaseModels
     public class BusinessIdeas
     {
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
-        public string? CreatorId { get; set; }
+        public ObjectId Id { get; set; }
+        public string CreatorId { get; set; }
+        public string CompanyName { get; set; }
+        public string Stage { get; set; }
+        public string ReadinessScore { get; set; }
+
+        // founder Identity
+        public FounderIdentity FounderIdentity { get; set; }
+
+        // problem
+        public Problem Problem { get; set; }
+
+
         public string Title { get; set; }
         public string Summary { get; set; }
-        public string Problem { get; set; }
+        //public string Problem { get; set; }
         public string Solution { get; set; }
         public string MarketSize { get; set; }
         public string RevenueModel { get; set; }
         public decimal FundingRequired { get; set; } // Use decimal for money
         public double EquityOffered { get; set; }
-        public string Stage { get; set; } // Idea | MVP | Growth
+        //public string Stage { get; set; } // Idea | MVP | Growth
         public string Status { get; set; } // Pending | Approved | Rejected
 
         public List<Milestone> Milestones { get; set; } = new();
@@ -32,7 +42,73 @@ namespace WebApp.Models.DatabaseModels
         // 3. Stage
     }
 
+    public class FounderIdentity
+    { 
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public string Experience { get; set; }
+        public bool LaunchedBefore { get; set; }
+        public string WeeklyTime { get; set; }
+        public string Motivation { get; set; }
+    }
 
+    public class Problem
+    { 
+        public string Description { get; set; }
+        public string AffectedUsers { get; set; }
+        public string CurrentSolutions  { get; set; }
+        public string gaps  { get; set; }
+    }
+
+    public class Solution
+    {
+        public string Description { get; set;}
+        public string CurrentSolutions { get; set; }
+        public string benefits { get; set; }
+        public string vision { get; set; }
+    }
+
+    public class Market
+    {
+        public string PrimaryCustomer { get; set; }
+        public string Geography { get; set; }
+        public string BuyingBehavior { get; set; }
+        public string MarketSize { get; set; }
+        public string Competitors { get; set; } // competitors: ["X", "Y"]
+    }
+
+   public class businessModel
+    {
+        public string FirstOffer { get; set; }
+        public string Pricing { get; set; }
+        public string SalesChannel { get; set; }
+        public string StatupCosts { get; set; }
+        public string Revenue12m { get; set; }
+        public string Partners { get; set; } // partners: ["Advisor A"]
+    }
+
+    public class Operations
+    {
+        public string Requirements { get; set; } // requirements: "Tools, team",
+        public string hasPrototype { get; set; }
+        public string tools { get; set; } // tools: ["Figma", "Stripe"],
+        public string risks { get; set; } // risks: ["Tech", "Legal"]
+    }
+
+    public class Roadmap
+    {
+        public string d30 { get; set; }
+        public string d90 { get; set; }
+        public string y1 { get; set; }
+    }
+
+    public class Compliance
+    {
+        public bool regulated { get; set; }
+        public string legalRisks { get; set; }
+        public string certifications { get; set; }
+        public string helpNeeded { get; set; }
+    }
     public class Milestone
     {
         public string Title { get; set; }
--- obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9ce79b9d8ae8369d96ceffd8bf6a638430cb9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe561b253e26f5971481b94163c777b19819a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
