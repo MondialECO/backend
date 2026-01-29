3c1813157150781969cc5dbf3b18de5126654a22
hafiz011 <hrhafij8@gmail.com>
2026-01-29 13:07:40 +0600

    backup

--- obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b117d8736beb048baf21d1756ee0658aeeac76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0116fe1771e2f582149a56ce36646636527b16c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
