3a9fb5400e961ad821c22c6630b9e84b517eb360
Md Hafizur Rahman <hrhafij8@gmail.com>
2026-04-14 23:43:21 +0600

    update auth

--- obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782ad2615fe83aac85be890cda7449c2670b9430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2678cd137ca35dcaec444929a5807d29de91d4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
